4634c2696dcf1babe640c4788814e1f80b5f3260
imoto-hirofumi <i.hirofumi724@gmail.com>
2020-09-04 01:49:19 +0900

    messageのhtmlの編集

---
+++ main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const carouselWidth = $("#carousel li").width(); 
+const length = $(".carousel-item").length; 
+const innerWidth = carouselWidth * length; 
+const time = 400;
+
+const carouselinner = $("#carousel");
+const prev = $("#prev");
+const next = $("#next");
+
+let c = 1;
+
+next.click(function () {
+    if (c == length) {
+        carouselinner.stop().animate({
+                left: 0,
+
+            },
+            time
+        );
+        c = 1;
+    } else {
+        carouselinner.stop().animate({
+                left: -c * carouselWidth,
+            },
+            time
+        );
+        c++;
+    }
+});
+
+prev.click(function () {
+    if (c == 1) {
+        carouselinner.stop().animate({
+                left: carouselWidth - innerWidth, 
+            },
+            time
+        );
+        c = length;
+
+    } else {
+        carouselinner.stop().animate({
+                left: -(c - 2) * carouselWidth,
+            },
+            time
+        );
+        c--;
+    }
+});
